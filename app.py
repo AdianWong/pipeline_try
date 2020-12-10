a57d1041b63c7653f9f530e3e0128b1e33745172
EC2 Default User <ec2-user@ip-172-31-3-128.ec2.internal>
2020-12-10 11:36:20 +0000

    Initial commit of application and pipeline

--- app.py
+++ app.py
@@ -3,9 +3,14 @@
 from aws_cdk import core
 
 from pipelines_webinar.pipelines_webinar_stack import PipelinesWebinarStack
+from pipelines_webinar.pipeline_stack import PipelineStack
+
 
 
 app = core.App()
 PipelinesWebinarStack(app, "pipelines-webinar")
-
+PipelineStack(app, 'PipelineStack', env={
+    'account': '881299606849',
+    'region': 'us-east-1'
+})
 app.synth()
