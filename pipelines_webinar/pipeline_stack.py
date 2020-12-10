56469d0a87ce2770d8fcec7e9a1906f94891469c
EC2 Default User <ec2-user@ip-172-31-3-128.ec2.internal>
2020-12-10 11:55:25 +0000

    add pre-prod stage

--- pipelines_webinar/pipeline_stack.py
+++ pipelines_webinar/pipeline_stack.py
@@ -2,7 +2,7 @@ from aws_cdk import core
 from aws_cdk import aws_codepipeline as codepipeline
 from aws_cdk import aws_codepipeline_actions as cpactions
 from aws_cdk import pipelines
-
+from .webservice_stage import WebServiceStage
 
 class PipelineStack(core.Stack):
   def init(self, scope: core.Construct, id: str, kwargs):
@@ -12,7 +12,7 @@ class PipelineStack(core.Stack):
     cloud_assembly_artifact = codepipeline.Artifact()
 
 
-    pipelines.CdkPipeline(self, 'Pipeline',
+    pipeline = pipelines.CdkPipeline(self, 'Pipeline',
       cloud_assembly_artifact=cloud_assembly_artifact,
       pipeline_name='WebinarPipeline',
 
@@ -30,3 +30,9 @@ class PipelineStack(core.Stack):
             cloud_assembly_artifact=cloud_assembly_artifact,
             install_command='npm install -g aws-cdk && pip install -r requirements.txt',
             synth_command='cdk synth'))
+            
+            
+    pipeline.add_application_stage(WebServiceStage(self, 'Pre-Prod', env={
+      'account': '881299606849',
+      'region': 'us-east-1'
+    }))
